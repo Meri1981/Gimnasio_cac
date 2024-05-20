a2f9e1170df9155df24b2d25f4c12638261b5483
jotapy79 <jppascale@hotmail.com>
2024-05-20 19:42:20 -0300

    Corrección del fuente mal mergeado

--- Gimnasio/web/forms.py
+++ Gimnasio/web/forms.py
@@ -5,22 +5,6 @@ from django.core.exceptions import ValidationError
 class RegistrarseForm(forms.Form):
     nombre = forms.CharField(label='Nombre', required=True)
     apellido = forms.CharField(label='Apellido', required=True)
-<<<<<<< HEAD
-    email = forms.EmailField(label='Email',required=True)
-    password = forms.CharField(label='Password',widget=forms.PasswordInput(), required=True)
-
-class ClaseForm(forms.Form):
-    id = forms.IntegerField(label='id')
-    id.widget.attrs.update({'readonly':True, 'class': "form-control"})
-    nombre = forms.CharField(label='Nombre', required=True)
-    nombre.widget.attrs.update({'class': 'form-control'})
-    profesor = forms.CharField(label='Profesor', required=True)
-    profesor.widget.attrs.update({'class': 'form-control'})
-    cupo = forms.IntegerField(label='Cupo', required=True, min_value=0, help_text="Cantidad de alumnos máximo por clase")
-    cupo.widget.attrs.update({'class': 'form-control'})
-    horario = forms.CharField(label="Horario", help_text="Horarios de la clase")
-    horario.widget.attrs.update({'class': 'form-control'})
-=======
     email = forms.EmailField(label='Email',required=True, validators=[EmailValidator(message="Ingrese un email válido")])
     password = forms.CharField(label='Password',widget=forms.PasswordInput(), required=True)
     confirmar_password= forms.CharField(label='Confirmar Password',widget=forms.PasswordInput(), required=True)
@@ -61,6 +45,16 @@ class ClaseForm(forms.Form):
           if password and confirmar_password and password != confirmar_password:
                 raise ValidationError("las contraseñas no coinciden")
           return self.cleaned_data
-          
-    
->>>>>>> bb0fa7c9fbb100cb0376376fefeb542df865cdf2
+
+class ClaseForm(forms.Form):
+    id = forms.IntegerField(label='id')
+    id.widget.attrs.update({'readonly':True, 'class': "form-control"})
+    nombre = forms.CharField(label='Nombre', required=True)
+    nombre.widget.attrs.update({'class': 'form-control'})
+    profesor = forms.CharField(label='Profesor', required=True)
+    profesor.widget.attrs.update({'class': 'form-control'})
+    cupo = forms.IntegerField(label='Cupo', required=True, min_value=0, help_text="Cantidad de alumnos máximo por clase")
+    cupo.widget.attrs.update({'class': 'form-control'})
+    horario = forms.CharField(label="Horario", help_text="Horarios de la clase")
+    horario.widget.attrs.update({'class': 'form-control'})
+

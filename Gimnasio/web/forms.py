bb0fa7c9fbb100cb0376376fefeb542df865cdf2
Meri1981 <mentaver16@gmail.com>
2024-05-20 18:59:14 -0300

    validación del formulario registro, agregado de campos, estilado en los css, comentario de registro exitoso

--- Gimnasio/web/forms.py
+++ Gimnasio/web/forms.py
@@ -1,7 +1,49 @@
 from django import forms
+from django.core.validators import EmailValidator
+from django.core.exceptions import ValidationError
 
 class RegistrarseForm(forms.Form):
     nombre = forms.CharField(label='Nombre', required=True)
     apellido = forms.CharField(label='Apellido', required=True)
-    email = forms.EmailField(label='Email',required=True)
+    email = forms.EmailField(label='Email',required=True, validators=[EmailValidator(message="Ingrese un email válido")])
     password = forms.CharField(label='Password',widget=forms.PasswordInput(), required=True)
+    confirmar_password= forms.CharField(label='Confirmar Password',widget=forms.PasswordInput(), required=True)
+
+
+
+    def clean_nombre(self):
+            if not self.cleaned_data["nombre"].isalpha():
+                raise ValidationError("El nombre solo puede estar compuesto por letras")
+
+            return self.cleaned_data["nombre"]
+    
+
+    
+    def clean_apellido(self):
+            if not self.cleaned_data["apellido"].isalpha():
+                raise ValidationError("El apellido solo puede estar compuesto por letras")
+
+            return self.cleaned_data["apellido"]
+    
+
+    def clean_password(self):
+          password = self.cleaned_data.get('password')
+          if len(password) < 8:
+                raise ValidationError('El password debe tener al menos 8 caracteres.')
+          if not any(char.isdigit() for char in password):
+                raise ValidationError('El password ebe tener al menos un número.')
+          if not any(char.isalpha() for char in password):
+                raise ValidationError('El password debe tener almenos una letra.')
+          return password
+    
+
+
+    def clean(self):
+          cleaned_data = super().clean()
+          password = cleaned_data.get('password')
+          confirmar_password = cleaned_data.get('confirmar_password')
+          if password and confirmar_password and password != confirmar_password:
+                raise ValidationError("las contraseñas no coinciden")
+          return self.cleaned_data
+          
+    
--- Gimnasio/web/views.py
+++ Gimnasio/web/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import *
+from django.shortcuts import redirect
+from django.contrib import messages
 
 
 # Create your views here.
 def index(request):
     context = {
-         'gimnasio' : 'Gimnasio CAC'
-    }
+         "gimnasio" : "Gimnasio CaC" }
     return render(request, "web/index.html", context)
+
+
 
 def registrarse(request):
     contexto ={}
     if request.method == 'GET':
             contexto['registrarse_form'] = RegistrarseForm()
     else: #asumimos que es un POST
-        contexto['registrarse_form'] = RegistrarseForm(request.POST)
+        form = RegistrarseForm(request.POST)
+        contexto['registrarse_form'] = form
+        if form.is_valid():
 
-        print(request.POST)
-        return redirect('index')
+            messages.success(request, "Se ha registrado con éxito")
+
+        #print(request.POST)
+            return redirect('index')
     return render(request, 'web/registrarse.html', contexto)
